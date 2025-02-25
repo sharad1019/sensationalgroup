a2bdb1bac01d6874dbe407db44d4e11a0437c31b
Sharad Kumar <78906216+sharad1019@users.noreply.github.com>
2025-02-25 22:53:26 +0530

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -548,3 +548,13 @@
 	});	
 
 })(window.jQuery);
+
+
+
+
+  let buttons = document.getElementsByClassName("btnstyleone");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      window.location.href = 'contact.html';
+    });
+  }
